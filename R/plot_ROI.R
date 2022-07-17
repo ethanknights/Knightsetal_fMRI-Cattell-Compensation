4995ee506a9586e13cc42bc0a237c9a245261069
ethanknights <ethanknights@hotmail.co.uk>
2022-07-17 08:23:30 +0100

    update scaling in univarite models - July

--- R/plot_ROI.R
+++ R/plot_ROI.R
@@ -1,30 +1,17 @@
-options(scipen = 999)
+options(scipen = 0)
 
-# df$Age_scale = as.numeric(scale(df$Age))
-# df$bhv_scale = as.numeric(scale(df$bhv))
-# df$Gender_scale = as.numeric(scale(df$Gender))
-# df$handedness_scale = as.numeric(scale(df$handedness))
-# df$lSPOC_scale = as.numeric(scale(df$lSPOC))
-# df$rMFG_scale = as.numeric(scale(df$rMFG))
-
-
-lm_model <- lm(scale(lSPOC) ~ scale(Age) * scale(bhv) + scale(Gender),
+#lm()
+lm_model <- lm(lSPOC ~ scale(Age) * scale(bhv) * scale(Gender),
                data = df); summary(lm_model)
 write.csv(as.data.frame(summary(lm_model)$coef), file=file.path('regression_ROI_cuneal.csv'))
 
-# lm_model <- lm(lSPOC ~ scale(Age) * scale(bhv),
-#                data = df); summary(lm_model)
-
-#Plot with fit (but no data points scatter)
-lm_model <- lm(lSPOC ~ Age * bhv + Gender,
+#Plot lm()
+lm_model <- lm(lSPOC ~ Age * bhv * Gender,
                data = df); summary(lm_model)
 p = plot_model(lm_model, type = "pred", terms = c("bhv", "Age [18, 54, 88]"), show.data = TRUE); p # vTert
-#formatting
-p <- p + #geom_hline(yintercept = 0.5) +
+p <- p +
   ylim(-1.5,0.9) +
   xlim(0,60) +
-  # scale_x_continuous(breaks = round(seq(20, max(80), by = 20),1),
-  #                    limits = c(15,90)) +
   theme_bw() +
   theme(panel.border = element_blank(),
         panel.grid.major = element_blank(),
@@ -40,13 +27,14 @@ ggsave(file.path(outImageDir,"lSPOC_sjPlot.png"),
 # #hack plot
 qq <- ggplot_build(p)
 # #hack plot - scatter
-qq$data[[1]]$shape <- o
-qq$data[[1]]$size <- 2.5
-qq$data[[1]]$stroke <- 1
-qq$data[[1]]$alpha <- 0.5
-qq$data[[1]]$colour[df$ageTert == 'YA'] = 'olivedrab'
-qq$data[[1]]$colour[df$ageTert == 'ML'] = 'darkturquoise'
-qq$data[[1]]$colour[df$ageTert == 'OA'] = 'dodgerblue3'
+qq$data[[1]]$shape <- 21
+qq$data[[1]]$size <- 3
+qq$data[[1]]$stroke <- 0.75
+qq$data[[1]]$colour = 'black'
+qq$data[[1]]$alpha <- 1
+qq$data[[1]]$fill[df$ageTert == 'YA'] = 'olivedrab'
+qq$data[[1]]$fill[df$ageTert == 'ML'] = 'darkturquoise'
+qq$data[[1]]$fill[df$ageTert == 'OA'] = 'dodgerblue3'
 # #hack plot - regression line
 qq$data[[2]]$size <- 2.5
 qq$data[[2]]$colour[1:7] = 'olivedrab'
@@ -57,55 +45,24 @@ qq$data[[3]]$alpha <- 0.2
 qq$data[[3]]$fill[1:7] = 'olivedrab'
 qq$data[[3]]$fill[8:14] = 'darkturquoise'
 qq$data[[3]]$fill[15:21] = 'dodgerblue3'
+#truncate (to match CI shading)
+qq$data[[2]]$y[21] = NA
+qq$data[[3]]$y[21] = NA
+qq$data[[3]]$ymin[21] = NA
+qq$data[[3]]$yax[21] = NA
+#replot
 plot(ggplot_gtable(qq))
 
 
-
-
-  
-#-------------- Get bayes factors --------------#
-#Cant use scale() in lmBF so scale first!
-df2 = df
-df2$age_scale = scale(df$Age)
-df2$bhv_scale = scale(df$bhv)
-df2$bhv_OutOfScanner_scale = scale(df$bhv_outOfScanner)
-df2$STW_scale = scale(df$STW)
-df2$lSPOC_scale = scale(df$lSPOC)
-df2$frontal_scale = scale(df$frontal)
-#extra
-df2$GenderNum_scale = scale(df$Gender)
-df2$handedness_scale = scale(df$handedness)
-
-full <-           lmBF(lSPOC_scale ~ Age_scale * bhv_scale + GenderNum_scale + handedness_scale,
-                       data = df2)
-noInteraction <-  lmBF(lSPOC_scale ~ Age_scale + bhv_scale + GenderNum_scale + handedness_scale,
-                       data = df2)
-bf10 = full / noInteraction #less than 1 favours models of 
-bf10
-
-
-
 #frontal
-lm_model <- lm(scale(frontal) ~ scale(Age) * scale(bhv) + scale(Gender),
+
+#lm()
+lm_model <- lm(frontal ~ scale(Age) * scale(bhv) * scale(Gender),
                data = df); summary(lm_model)
 write.csv(as.data.frame(summary(lm_model)$coef), file=file.path('regression_ROI_frontal.csv'))
-#lm_model <- lm(frontal ~ scale(Age) * scale(bhv),
-#               data = df); summary(lm_model)
 
 
-full <-           lmBF(frontal_scale ~ Age_scale * bhv_scale + GenderNum_scale + handedness_scale,
-                       data = df2)
-noInteraction <-  lmBF(frontal_scale ~ Age_scale + bhv_scale + GenderNum_scale + handedness_scale,
-                       data = df2)
-bf10 = full / noInteraction #less than 1 favours models of 
-bf01 = 1/bf10; bf01
-
-
-
-
-
-
-#Plot with fit
+#Plot lm()
 lm_model <- lm(frontal ~ Age * bhv + Gender,
                data = df); summary(lm_model)
 p = plot_model(lm_model, type = "pred", terms = c("bhv", "Age [18, 54, 88]"), show.data = TRUE); p # vTert
@@ -113,8 +70,6 @@ p = plot_model(lm_model, type = "pred", terms = c("bhv", "Age [18, 54, 88]"), sh
 p <- p +
   ylim(-1.5,0.9) +
   xlim(0,60) +
-  # scale_x_continuous(breaks = round(seq(20, max(80), by = 20),1),
-  #                    limits = c(15,90)) +
   theme_bw() +
   theme(panel.border = element_blank(),
         panel.grid.major = element_blank(),
@@ -147,5 +102,3 @@ qq$data[[3]]$fill[1:7] = 'olivedrab'
 qq$data[[3]]$fill[8:14] = 'darkturquoise'
 qq$data[[3]]$fill[15:21] = 'dodgerblue3'
 plot(ggplot_gtable(qq))
-
-
--- R/plot_bhv.R
+++ R/plot_bhv.R
@@ -1,31 +1,27 @@
 options(scipen = 0)
-#put relevant stuff in wide and long format
+
 wide <- data.frame(
-  CCID = df$CCID,
-  age = df$Age,
-  bhv.STW = scale(df$STW),
-  bhv.outScanner = scale(df$bhv_outOfScanner),
-  bhv.inScanner = scale(df$bhv),
-  GenderNum = scale(df$Gender), #-1.0022 is male
-  handedness = scale(df$handedness),
-  ageTert = df$ageTert
+  CCID                  = df$CCID,
+  age                   = df$Age,
+  age_scale             = scale(df$Age),
+  bhv.outScanner        = df$bhv_outOfScanner,
+  bhv.outScanner_scale  = scale(df$bhv_outOfScanner),
+  bhv.inScanner         = df$bhv,
+  bhv.inScanner_scale   = scale(df$bhv),
+  GenderNum             = df$Gender,
+  GenderNum_scale       = scale(df$Gender), #-1.0022 is male
+  ageTert               = df$ageTert
 )
-long <- wide %>% gather(Key, Performance, bhv.outScanner, bhv.inScanner, bhv.STW)#, GenderNum, handedness)
 
-wide$ageLin_scale = scale(wide$age)
-wide$ageLinQuad_scale = poly(scale(wide$age),2)
-#wide$ageQuadOnly_scale = wide$ageLin_scale ^ 2 #doesnt orthogonalise like poly!
 
-#=====================================
-#inscanner
+#inScanner
 #=====================================
 
 #lm()
 #-------------------------------------
-lm_model <- lm(bhv.inScanner ~ ageLinQuad_scale * GenderNum, #these are all already scaled!
+lm_model <- lm(bhv.inScanner ~ age_scale * GenderNum_scale,
                data = wide); summary(lm_model)
 write.csv(as.data.frame(summary(lm_model)$coef), file=file.path(outImageDir,'regression_bhv.csv'))
-#p = plot_model(lm_model, type = 'pred', terms = c('GenderNum', 'ageLinQuad_scale'), show.data = TRUE); p
 
 #Plot lm()
 #-------------------------------------
@@ -33,27 +29,24 @@ lm_model <- lm(bhv.inScanner ~ age * factor(GenderNum),
                data = wide); summary(lm_model)
 p = plot_model(lm_model, type = 'pred', terms = c('age','GenderNum'), show.data = TRUE); p
 p = p +
-  ylim(-4.5,2) + 
+  ylim(0,60) + 
   scale_x_continuous(breaks = round(seq(20, max(80), by = 20),1),
                      limits = c(15,90)) +
   theme_bw() + 
-  theme(panel.border = element_blank(),
-        panel.grid.major = element_blank(),
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         legend.position = 'none', #"right",
-        panel.grid.minor = element_blank(),
-        axis.line = 
-          element_line(colour = "black",size = 1.5), 
-        axis.ticks = element_line(colour = "black",
-                                  size = 1.5),
+        axis.line = element_line(colour = "black", size = 1.5), 
+        axis.ticks = element_line(colour = "black", size = 1.5),
         text = element_text(size=24)); p
 qq <- ggplot_build(p)
 # #hack plot - scatter
-#qq$data[[1]]$shape <- o
-qq$data[[1]]$size <- 2.5
-qq$data[[1]]$stroke <- 2
-qq$data[[1]]$alpha <- 0.5
-qq$data[[1]]$colour[round(wide$GenderNum) == 1] = 'orange'
-qq$data[[1]]$colour[round(wide$GenderNum) == -1] = 'purple'
+qq$data[[1]]$shape = 21
+qq$data[[1]]$stroke <- 0.75
+qq$data[[1]]$fill[round(wide$GenderNum) == 2] = 'orange'
+qq$data[[1]]$fill[round(wide$GenderNum) == 1] = 'purple'
+qq$data[[1]]$colour = 'black'
+qq$data[[1]]$alpha = 0.6
+qq$data[[1]]$size = 3
 # #hack plot - regression line
 qq$data[[2]]$size <- 2
 qq$data[[2]]$colour[1:9] = 'orange'
@@ -63,117 +56,33 @@ qq$data[[3]]$alpha <- 0.2
 qq$data[[3]]$fill[1:9] = 'orange'
 qq$data[[3]]$fill[10:18] = 'purple'
 plot(ggplot_gtable(qq))
-#ggsave(file.path(outImageDir,"bhv_fluid_withGenderInteraction.png"),
-#       width = 25, height = 25, units = 'cm', dpi = 300); p
-
-#BF10
-#-------------------------------------
-full <- lmBF(bhv.inScanner ~ ageLin_scale + GenderNum,
-               data = wide)
-part <- lmBF(bhv.inScanner ~ GenderNum,
-             data = wide)
-bf10 = full / part; bf10  #1/full
-bf01 = 1/bf10; bf01
-
-full <- lmBF(bhv.inScanner ~ ageLin_scale + GenderNum,
-             data = wide)
-part <- lmBF(bhv.inScanner ~ ageLin_scale * GenderNum,
-             data = wide)
-bf10 = full / part; bf10  #1/full
-bf01 = 1/bf10; bf01
-
+library(grid)
+grid.draw(ggplot_gtable(qq))
+dev.copy(device = tiff, filename = file.path(outImageDir,'bhv_plot.tiff'),width = 1000, height = 600); dev.off()
 
 #outscanner
 #====================================
-lm_model <- lm(bhv.outScanner ~ ageLinQuad_scale * GenderNum,
+
+#lm()
+#------------------------------------
+lm_model <- lm(bhv.outScanner ~ age_scale * GenderNum_scale,
                data = wide); summary(lm_model)
 write.csv(as.data.frame(summary(lm_model)$coef), file=file.path(outImageDir,'regression_bhv-outScanner.csv'))
 
-# in/out-of-scanner correlation
-cor.test(x = wide$bhv.inScanner, y = wide$bhv.outScanner, method = 'pearson', alternative = 'greater') #predict positive
-#cor.test(x = df$bhv,df$bhv_outOfScanner, method = 'pearson', alternative = 'greater')
-
-full <- correlationBF(x = wide$bhv.inScanner, y = wide$bhv.outScanner)
-1 / full
-
-#ICC
-library(irr)
-# #Zscore version (shoudlnt use as losing info from mean/stdev)!
-# icc(
-#   wide[c('bhv.inScanner','bhv.outScanner')],
-#     model = "oneway", type = "agreement", unit = "single")
-#Raw data ICC
-icc(
-  df[c('bhv','bhv_outOfScanner')],
-  model = "oneway", type = "agreement", unit = "single")
-
+#in/out-of-scanner correlation
+#------------------------------------
+cor.test(x = wide$bhv.inScanner_scale, y = wide$bhv.outScanner_scale, method = 'pearson', alternative = 'greater') #predict positive
 
 #plot in-out correlation
-p <- ggplot(wide, aes(x = bhv.inScanner, y = bhv.outScanner, colour = age))
-p <- p + geom_point(shape = 16, size = 5, stroke = 2, alpha = 0.5) +
+p <- ggplot(wide, aes(x = bhv.inScanner_scale, y = bhv.outScanner_scale, fill = age))
+p <- p + geom_point(shape = 21, size = 3, stroke = 1.25, alpha = 1, colour = 'black') +
   geom_abline(intercept = 0, size = 2) +
   ylim(-4.05,2.1) + xlim(-4.05,2.1) +
-  scale_color_gradient(low="blue", high="red")
-p = p +
+  scale_fill_gradient(low="green", high="blue") +
   theme_bw() + 
-  theme(panel.border = element_blank(),
-        panel.grid.major = element_blank(),
-        legend.position = 'right', #"right",
-        panel.grid.minor = element_blank(),
-        axis.line = 
-          element_line(colour = "black",size = 1.5), 
-        axis.ticks = element_line(colour = "black",
-                                  size = 1.5),
-        text = element_text(size=24));
-ggsave(file.path(outImageDir,"bhv_correlation.png"),
-       width = 25, height = 25, units = 'cm', dpi = 300); p
-
-# 
-# #######################################################
-# ## STW
-# long_fluid = long[!long$Key == 'bhv.STW',]
-# write.csv(long_fluid,'tmp_long_fluid.csv')
-# 
-# lm_model <- lm(bhv.STW ~ ageLin_scale,
-#                data = wide); summary(lm_model)
-# lm_model <- lm(bhv.STW ~ ageLinQuad_scale + GenderNum + handedness,
-#                data = wide); summary(lm_model)
-# 
-# #
-# long_crystal = long[long$Key == 'bhv.STW',]
-# 
-# p <- ggplot(long_crystal, aes(x = age, y = Performance))
-# p <- p + geom_point(shape = 20, size = 4, stroke = 2, alpha = 0.75, colour = 'lightpink') +
-#   stat_smooth(method = "lm", size = 2, fill = 'grey60', colour = 'lightpink') +
-#   ylim(-6,2) +
-#   scale_x_continuous(breaks = round(seq(20, max(80), by = 20),1),
-#                      limits = c(15,90)) +
-#   theme_bw() + 
-#   theme(panel.border = element_blank(),
-#         panel.grid.major = element_blank(),
-#         legend.position = 'right', #"right",
-#         panel.grid.minor = element_blank(),
-#         axis.line = 
-#           element_line(colour = "black",size = 1.5), 
-#         axis.ticks = element_line(colour = "black",
-#                                   size = 1.5),
-#         text = element_text(size=24));
-# ggsave(file.path(outImageDir,"bhv_crystal.png"),
-#        width = 25, height = 25, units = 'cm', dpi = 300); p
-# 
-# 
-# 
-# #Plot subject CFIT joined timepoints
-# long2 = long <- data.frame(
-#   bhv.outScanner = scale(df$bhv_outOfScanner),
-#   bhv.inScanner = scale(df$bhv),
-#   age = df$Age
-# )
-# long2 <- long2 %>% gather(Key, Performance, bhv.outScanner, bhv.inScanner)
-# 
-# p <- ggplot(long2, aes(x = age, y = Performance, colour = Key, fill = Key))
-# p <- p + geom_point(shape = 21,size = 4, colour = 'black', stroke = 2, alpha = 0.75)
-# p
-# 
-# 
-# 
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        legend.position = "right",
+        axis.line = element_line(colour = "black", size = 1.5), 
+        axis.ticks = element_line(colour = "black", size = 1.5),
+        text = element_text(size=24)); p
+p
