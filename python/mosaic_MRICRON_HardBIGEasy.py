a0d16a806184f3379c60928799b800b854f9d12d
ethanknights <ethanknights@hotmail.co.uk>
2022-08-18 22:26:43 +0100

    add python GL plotting

---
+++ python/mosaic_MRICRON_HardBIGEasy.py
@@ -0,0 +1,20 @@
+import gl
+gl.resetdefaults()
+
+#open background image
+gl.loadimage('spm152')
+
+#open overlay: show positive regions
+gl.overlayload('./nii_HardBIGEasy/Intercept_tfce197.nii')
+gl.minmax(2, 0, 10000)
+gl.colorname (1,"1red")
+
+#gl.moasic
+#-----------------
+#L+                     labels on
+#first value            slice overlap
+#S X R 0                surface cross section with mosaics' slice lines
+#gl.mosaic("A H -0.1 -6 0 6 12 18; 24 30 36 42 48")
+
+gl.mosaic("A H -0.1 V -0.1 -8 0 8 16 24; 28 32 40 48 54")
+
