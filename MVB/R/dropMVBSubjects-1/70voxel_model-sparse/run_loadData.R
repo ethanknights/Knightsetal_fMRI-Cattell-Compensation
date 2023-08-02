691130972a934d93681519e2115c6a8ccb26ae2c
ethanknights <ethanknights@hotmail.co.uk>
2023-08-02 18:12:08 +0100

    tidy comments

--- MVB/R/dropMVBSubjects-1/70voxel_model-sparse/run_loadData.R
+++ MVB/R/dropMVBSubjects-1/70voxel_model-sparse/run_loadData.R
@@ -43,12 +43,12 @@ rawD <- read.csv(file.path(rawDir,'MVB_cuneal.csv'), header=TRUE,sep=",")
 df = rawD
 
 #---- Transform some stuff ----#
-#make variablers factors
+#make variables factors
 df$ordy <- as.factor(df$ordy) #Check it worked: sapply(df, class)
 
 #---- assign subs a tertile age group in df$tert ----#
 # Find tertiles
-vTert = quantile(df$age, c(0:3/3)) #rememebr this is useful for plot_model
+vTert = quantile(df$age, c(0:3/3)) #remember this is useful for plot_model
 
 df$ageTert = with(df, 
                   cut(age, 
@@ -61,7 +61,5 @@ df_univariate = read.csv('../../../../R/csv/univariate.csv')
 df <- merge(df, df_univariate, by = c("CCID", "CCID"))
 
 ## RUN ANALYSES:
-# source('run_fmRI_MVB_covaryUnivariate.R')
-# run_fMRI_spread.R
 # run_fMRI_MVB.R
 # run_fMRI_MVB_predict_Behaviour.R
